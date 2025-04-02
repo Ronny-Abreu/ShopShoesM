12c04229709d6d6510c0c46e1956455bb081e85f
SlimDReaper <ronnyduran35@gmail.com>
2025-04-02 16:49:35 -0400

    Creación de la conexión a la base de datos SQL Server

---
+++ ShopShoesMale/ConexionDB.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Data.SqlClient
+
+Module ConexionDB
+    Public conexion As New SqlConnection("Server=SLIMREAPER;Database=ShopShoesM;Trusted_Connection=True;")
+
+    Public Sub AbrirConexion()
+        Try
+            If conexion.State = ConnectionState.Closed Then
+                conexion.Open()
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error de conexión: " & ex.Message)
+        End Try
+    End Sub
+
+    Public Sub CerrarConexion()
+        If conexion.State = ConnectionState.Open Then
+            conexion.Close()
+        End If
+    End Sub
+End Module
