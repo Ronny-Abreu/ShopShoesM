64520197a1345e6c3489e60d48acf58e6ce0aadc
SlimDReaper <ronnyduran35@gmail.com>
2025-04-02 22:45:02 -0400

    Creation LoginForm

---
+++ ShopShoesMale/LoginForm.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class LoginForm
+    Inherits System.Windows.Forms.Form
+
+    ' Limpieza de componentes
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    ' Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    ' Inicialización de componentes
+    Private Sub InitializeComponent()
+        Me.lblCorreo = New System.Windows.Forms.Label()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnIniciarSesion = New System.Windows.Forms.Button()
+        Me.lblNoTengoCuenta = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblCorreo
+        '
+        Me.lblCorreo.AutoSize = True
+        Me.lblCorreo.Location = New System.Drawing.Point(267, 242)
+        Me.lblCorreo.Name = "lblCorreo"
+        Me.lblCorreo.Size = New System.Drawing.Size(61, 20)
+        Me.lblCorreo.TabIndex = 0
+        Me.lblCorreo.Text = "Correo:"
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(367, 242)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(200, 26)
+        Me.txtCorreo.TabIndex = 1
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(267, 292)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(96, 20)
+        Me.lblPassword.TabIndex = 2
+        Me.lblPassword.Text = "Contraseña:"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(367, 292)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(200, 26)
+        Me.txtPassword.TabIndex = 3
+        Me.txtPassword.UseSystemPasswordChar = True
+        '
+        'btnIniciarSesion
+        '
+        Me.btnIniciarSesion.Location = New System.Drawing.Point(402, 364)
+        Me.btnIniciarSesion.Name = "btnIniciarSesion"
+        Me.btnIniciarSesion.Size = New System.Drawing.Size(120, 35)
+        Me.btnIniciarSesion.TabIndex = 4
+        Me.btnIniciarSesion.Text = "Iniciar Sesión"
+        Me.btnIniciarSesion.UseVisualStyleBackColor = True
+        '
+        'lblNoTengoCuenta
+        '
+        Me.lblNoTengoCuenta.AutoSize = True
+        Me.lblNoTengoCuenta.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblNoTengoCuenta.ForeColor = System.Drawing.Color.Blue
+        Me.lblNoTengoCuenta.Location = New System.Drawing.Point(309, 395)
+        Me.lblNoTengoCuenta.Name = "lblNoTengoCuenta"
+        Me.lblNoTengoCuenta.Size = New System.Drawing.Size(238, 20)
+        Me.lblNoTengoCuenta.TabIndex = 9
+        Me.lblNoTengoCuenta.Text = "¿No tienes cuenta? Registrate aquí"
+        '
+        'LoginForm
+        '
+        Me.ClientSize = New System.Drawing.Size(883, 598)
+        Me.Controls.Add(Me.lblCorreo)
+        Me.Controls.Add(Me.txtCorreo)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.btnIniciarSesion)
+        Me.Name = "LoginForm"
+        Me.Text = "Inicio de Sesión"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblCorreo As Label
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnIniciarSesion As Button
+    Friend WithEvents lblNoTengoCuenta As Label
+End Class
---
+++ ShopShoesMale/LoginForm.vb
@@ -0,0 +1,53 @@
+﻿
+' Función para verificar los datos de login
+Imports System.Data.SqlClient
+
+Public Class LoginForm
+    Private conexion As New SqlConnection("Server=SLIMREAPER;Database=ShopShoesM;Trusted_Connection=True;")
+
+    Private Sub btnIniciarSesion_Click(sender As Object, e As EventArgs) Handles btnIniciarSesion.Click
+        Dim correo As String = txtCorreo.Text
+        Dim password As String = txtPassword.Text
+
+        If VerificarUsuario(correo, password) Then
+
+            ' Abrir HomeForm y ocultar este formulario
+            Dim home As New HomeForm()
+            home.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Correo o contraseña incorrectos.")
+        End If
+    End Sub
+
+    ' Función para verificar usuario en la base de datos
+    Private Function VerificarUsuario(correo As String, password As String) As Boolean
+        Dim usuarioEncontrado As Boolean = False
+        Try
+            conexion.Open()
+            Dim cmd As New SqlCommand("SELECT * FROM Usuarios WHERE Correo = @correo AND Password = @password", conexion)
+            cmd.Parameters.AddWithValue("@correo", correo)
+            cmd.Parameters.AddWithValue("@password", password)
+
+            Dim reader As SqlDataReader = cmd.ExecuteReader()
+            If reader.HasRows Then
+                usuarioEncontrado = True
+            End If
+
+            reader.Close()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            conexion.Close()
+        End Try
+
+        Return usuarioEncontrado
+    End Function
+
+    'Función para redirigir al formulario de registrarse si el usuario quiere ir allá'
+    Private Sub lblNoTengoCuenta_Click(sender As Object, e As EventArgs) Handles lblNoTengoCuenta.Click
+        Dim Register As New Register()
+        Register.Show()
+        Me.Hide() ' Oculta el formulario de registro
+    End Sub
+End Class
