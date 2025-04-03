d71a0b3bf64dc63b191934c8a34fe76b6ae35fe5
SlimDReaper <ronnyduran35@gmail.com>
2025-04-02 23:59:06 -0400

    Creation initial HomeForm

--- ShopShoesMale/HomeForm.Designer.vb
+++ ShopShoesMale/HomeForm.Designer.vb
@@ -8,26 +8,50 @@ Partial Class HomeForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lblMensaje = New System.Windows.Forms.Label()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblMensaje
         '
-        Me.lblMensaje.AutoSize = True
-        Me.lblMensaje.Font = New System.Drawing.Font("Arial", 14.0!, System.Drawing.FontStyle.Bold)
-        Me.lblMensaje.Location = New System.Drawing.Point(337, 242)
+        Me.lblMensaje.Location = New System.Drawing.Point(20, 50)
         Me.lblMensaje.Name = "lblMensaje"
-        Me.lblMensaje.Size = New System.Drawing.Size(277, 33)
-        Me.lblMensaje.TabIndex = 0
-        Me.lblMensaje.Text = "Has iniciado sesión"
+        Me.lblMensaje.Size = New System.Drawing.Size(341, 34)
+        Me.lblMensaje.TabIndex = 2
+        Me.lblMensaje.Text = "Bienvenido a ShopShoesM"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(937, 30)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(115, 25)
+        Me.ToolStripLabel1.Text = "Iniciar Sesión"
         '
         'HomeForm
         '
         Me.ClientSize = New System.Drawing.Size(937, 585)
+        Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.lblMensaje)
         Me.Name = "HomeForm"
-        Me.Text = "Inicio"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Home"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
 End Class
--- ShopShoesMale/HomeForm.vb
+++ ShopShoesMale/HomeForm.vb
@@ -1,3 +1,18 @@
 ﻿Public Class HomeForm
+    ' Variable compartida para controlar la sesión
+    Public Shared UsuarioLogueado As Boolean = False
 
+    Private Sub HomeForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Si el usuario ya ha iniciado sesión, oculta el botón de "Iniciar sesión"
+        If UsuarioLogueado Then
+            ToolStripLabel1.Visible = False
+        End If
+    End Sub
+
+    ' Evento para redirigir al formulario de login
+    Private Sub ToolStripLabel1_Click(sender As Object, e As EventArgs) Handles ToolStripLabel1.Click
+        Dim login As New LoginForm()
+        login.Show()
+        Me.Hide()
+    End Sub
 End Class
