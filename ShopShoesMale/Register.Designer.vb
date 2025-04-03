08ebc7fc749e3b471127a46366b129bf2d023ac8
SlimDReaper <ronnyduran35@gmail.com>
2025-04-02 22:44:51 -0400

    Update Designer of Register

---
+++ ShopShoesMale/Register.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Register
+    Inherits System.Windows.Forms.Form
+
+    ' Limpieza de componentes
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    ' Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    ' Inicialización de componentes
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblCorreo = New System.Windows.Forms.Label()
+        Me.lblTelefono = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.lblTengoCuenta = New System.Windows.Forms.Label()
+        Me.btnRegistrar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(303, 203)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(241, 26)
+        Me.txtNombre.TabIndex = 0
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(211, 203)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(69, 20)
+        Me.lblNombre.TabIndex = 1
+        Me.lblNombre.Text = "Nombre:"
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(303, 253)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(241, 26)
+        Me.txtCorreo.TabIndex = 2
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(303, 303)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(241, 26)
+        Me.txtTelefono.TabIndex = 3
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(306, 356)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(241, 26)
+        Me.txtPassword.TabIndex = 4
+        Me.txtPassword.UseSystemPasswordChar = True
+        '
+        'lblCorreo
+        '
+        Me.lblCorreo.AutoSize = True
+        Me.lblCorreo.Location = New System.Drawing.Point(211, 255)
+        Me.lblCorreo.Name = "lblCorreo"
+        Me.lblCorreo.Size = New System.Drawing.Size(61, 20)
+        Me.lblCorreo.TabIndex = 5
+        Me.lblCorreo.Text = "Correo:"
+        '
+        'lblTelefono
+        '
+        Me.lblTelefono.AutoSize = True
+        Me.lblTelefono.Location = New System.Drawing.Point(211, 306)
+        Me.lblTelefono.Name = "lblTelefono"
+        Me.lblTelefono.Size = New System.Drawing.Size(75, 20)
+        Me.lblTelefono.TabIndex = 6
+        Me.lblTelefono.Text = "Teléfono:"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(211, 359)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(96, 20)
+        Me.lblPassword.TabIndex = 7
+        Me.lblPassword.Text = "Contraseña:"
+        '
+        'lblTengoCuenta
+        '
+        Me.lblTengoCuenta.AutoSize = True
+        Me.lblTengoCuenta.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblTengoCuenta.ForeColor = System.Drawing.Color.Blue
+        Me.lblTengoCuenta.Location = New System.Drawing.Point(309, 395)
+        Me.lblTengoCuenta.Name = "lblTengoCuenta"
+        Me.lblTengoCuenta.Size = New System.Drawing.Size(238, 20)
+        Me.lblTengoCuenta.TabIndex = 9
+        Me.lblTengoCuenta.Text = "¿Ya tienes cuenta? Inicia sesión"
+        '
+        'btnRegistrar
+        '
+        Me.btnRegistrar.Location = New System.Drawing.Point(369, 514)
+        Me.btnRegistrar.Name = "btnRegistrar"
+        Me.btnRegistrar.Size = New System.Drawing.Size(121, 35)
+        Me.btnRegistrar.TabIndex = 8
+        Me.btnRegistrar.Text = "Registrar"
+        Me.btnRegistrar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(943, 634)
+        Me.Controls.Add(Me.btnRegistrar)
+        Me.Controls.Add(Me.lblTelefono)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblTengoCuenta)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblCorreo)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtCorreo)
+        Me.Name = "Form1"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblCorreo As Label
+    Friend WithEvents lblTelefono As Label
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents btnRegistrar As Button
+    Private WithEvents lblTengoCuenta As Label
+End Class
